5d666b86cb65db667b05405e8228a23b1f609c1f
Mani-mka <manishsachin.mk7514@gmail.com>
2021-08-11 02:20:56 +0530

    main file try catch

--- src/com/control/HibernateMain.java
+++ src/com/control/HibernateMain.java
@@ -10,17 +10,21 @@ import org.hibernate.cfg.Configuration;
 public class HibernateMain {
     public static void main(String[] args) {
 
-        Configuration con = new Configuration().configure();
-        con.addAnnotatedClass(com.to.Customer.class);
-        StandardServiceRegistryBuilder builder = new
-                StandardServiceRegistryBuilder().applySettings(con.getProperties());
-        SessionFactory factory = con.buildSessionFactory(builder.build());
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Customer c1 = new Customer(1, "Manish", "Oven", 3000);
-        session.save(c1);
-        transaction.commit();
-        session.close();
+        try {
+            Configuration con = new Configuration().configure();
+            con.addAnnotatedClass(com.to.Customer.class);
+            StandardServiceRegistryBuilder builder = new
+                    StandardServiceRegistryBuilder().applySettings(con.getProperties());
+            SessionFactory factory = con.buildSessionFactory(builder.build());
+            Session session = factory.openSession();
+            Transaction transaction = session.beginTransaction();
+            Customer c1 = new Customer(1, "Manish", "Oven", 3000);
+            session.save(c1);
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
